06261cd87ff302390c1f78f192642aca52baf1d1
Selvakumar-18 <selvakumarbalasubramani4@gmail.com>
2024-01-18 10:40:06 +0530

    firstcommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'localstorage';
+  getAllData:any;
+  arrObj:any[]=[];
+  saveSData:any;
+  constructor(private appService:AppService){}
+  ngOnInit(){}
+
+  getObj(names:any,gmail:any){
+    const temp = {nameS:names.value,gmail:gmail.value}
+    
+     this.arrObj.push(temp)
+    console.log(this.arrObj )
+    this.saveData(this.arrObj)
+  }
+  saveData(data:any){
+    this.saveSData = data
+    this.appService.setDataLocal('useData',this.saveSData)
+    this.getData()
+  }
+
+  getData(){
+    let getDatas = localStorage.getItem('userData');
+    console.log('getDta',getDatas)
+  }
 }
